1a58d1e4e31473a0be34cbdb09c1997461f8e600
eux <ixue@163.com>
2016-06-20 23:38:43 +0800

    Build sliders switch automatically

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,39 @@
 
 $(function() {
-	// next();
+	startSlide();
+	$(".previous").click(previous);
+	$(".next").click(next);
+	$(".circle").click(circle);
 });
+
+let sliderIni = 1;
+let sliderNext = 1;
+let count = $(".slider").size();
+
+function startSlide() {
+	if (sliderNext > count) {
+		sliderIni = 1;
+		sliderNext = 1;
+	}
+	$(".slider").fadeOut(400);
+	$(".image-" + sliderNext).fadeIn(2000);
+	$(".dot").removeClass("dot-focus");
+	$(".circle-" + sliderNext).addClass("dot-focus");
+	sliderNext++;
+
+	setTimeout(startSlide, 5000);
+}
 
 function next() {
 	let imgNum;
-	$(".slide").removeClass("image-focus");
 	imgNum = $(".image-focus").eq();
 	console.log(imgNum);
+}
+
+function previous() {
+
+}
+
+function circle() {
+
 }
